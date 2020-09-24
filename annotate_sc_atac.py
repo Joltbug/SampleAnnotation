9265ea9d2ec21e018fbf0e75bccaa377bfb91093
Joltbug <Jxh665@gmail.com>
2020-09-24 00:25:30 -0400

    added MultiProcessing and .yaml file support

--- annotate_sc_atac.py
+++ annotate_sc_atac.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[23]:
-
-
 import os
 import seaborn as sns
 
@@ -18,16 +15,21 @@ import csv
 
 import scipy
 
-
-# In[2]:
-
-
+import yaml
 import getopt, sys
+
+import scipy.stats as stats
+
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+from multiprocessing import Pool
+
 
 full_cmd_arguments = sys.argv
 argument_list = full_cmd_arguments[1:]
-short_options = "i:r:b:fvo"
-long_options = ["inpu=", "ref_folder=", "background=","figure", "verbose", "output"]
+short_options = "i:r:b:f:vom"
+long_options = ["input=", "ref_folder=", "background=","figure_cols=", "verbose", "output","multiple_processing"]
 
 try:
     arguments, values = getopt.getopt(argument_list, short_options, long_options)
@@ -41,20 +43,21 @@ inP = False
 outP = False
 ref = False
 bkg = False
+multiple = False
     
 for a, v in arguments:
     if a in ("-v", "--verbose"):
         verbose = True
-    elif a in ("-f", "--figure"):
-        figure = True
+    elif a in ("-f", "--figure_cols"):
+        figure = v
+    elif current_argument in ("-m", "--multiple_processing"):
+        multiple = True
     elif current_argument in ("-o", "--output"):
         outP = True
     elif current_argument in ("-i", "--input"):
         inP = v
     elif current_argument in ("-r", "--ref_folder"):
         ref = v
-    elif current_argument in ("-b", "--background="):
-        bkg = v
 
 if (not inP):
     print("Missing input file")
@@ -62,33 +65,15 @@ if (not inP):
 if (not bkg):
     print("Missing background file")
     sys.exit(2)
+if (not ref):
+    print("Missing reference folder address")
+    sys.exit(2)
 
-
-# In[3]:
-
-
-#manually set arguments for use in ipynb
-inP = "demo_data.csv"
-bkg = "GSE97887_peaks_Occ1_Fcx1_Cbh1_joint_spp_RMMMcombined_RMrepeatmask100_bandwidth500_step100_thr5_span10_fdr1e-07.sorted.bed"
-verbose = True
-Figure = True
-outP = True
-
-
-# In[4]:
 
 
 if(verbose): print("Reading Input File")
 
-
-# In[5]:
-
-
 data = pd.read_csv(inP)
-
-
-# In[6]:
-
 
 data.rename( columns={'Unnamed: 0':'id2peak'}, inplace=True)
 id2peak = []
@@ -97,50 +82,35 @@ for c in data['id2peak']:
     id2peak.append((temp[0], int(temp[1]), int(temp[2])))
 
 id2peak.sort( key = lambda x: (x[0], x[1]))
-#id2peak.sort(key=lambda x: x[1])
-#id2peak = np.array(id2peak, dtype=object)
 
-
-# In[7]:
-
+with open((ref + 'info.yaml')) as reffile: refinfo = yaml.load(reffile,Loader=yaml.FullLoader)
+bkg = ref + refinfo['background']
+if (verbose):
+    print('Using background file: ',refinfo['background'])
+    print('With cell types:')
+    for cell in refinfo['cell_type']:
+        print (cell,':',refinfo['cell_type'][cell])
 
 if(verbose): print("Calculating Bed Overlaps")
 
-
-# In[8]:
-
-
-#fn_bk1 = 'GSE97887_peaks_Occ1_Fcx1_Cbh1_joint_spp_RMMMcombined_RMrepeatmask100_bandwidth500_step100_thr5_span10_fdr1e-07.sorted.bed'
-fn_bk1_data = bdO.BedScan(bkg, "\t")
-
+fn_bk1_data = bdO.BedScanPd(bkg)
 intersect = bdO.BedOverlap(id2peak, fn_bk1_data, 50)
 
-id2target = ['Ex', 'In', 'Mic', 'End', 'Ast', 'Opc', 'Oli']
-if(ref): tmp = ref + '/GSE97887_occ1_MAINSPLITS.%s.diffPeaks.sorted.bed'
-else: tmp = 'GSE97887_occ1_MAINSPLITS.%s.diffPeaks.sorted.bed'
-fn_target = [(c, tmp % c)  for c in id2target]
+fn_target = [(c, ref + refinfo['cell_type'][c]) for c in refinfo['cell_type']]
+
+
 
 set2_ref = []
 
 for c, f1 in fn_target:
-    scan = bdO.BedScan(f1, "\t")
+    scan = bdO.BedScanPd(f1)
     s2 = bdO.BedOverlap(id2peak, scan, 50)
     set2_ref.append((c, s2))
-    
-
-
-# In[9]:
-
 
 if(verbose): print("Calculating Fischer Exact Scores")
 
-
-# In[69]:
-
-
-import scipy.stats as stats
 def get_fisher_exact(s1, s2, sbk):
-    `
+    
     n1 = len(s1 & s2)
     n2 = len(s1 & (sbk - s2))
     n3 = len((sbk - s1) & s2)
@@ -149,46 +119,47 @@ def get_fisher_exact(s1, s2, sbk):
     mat = [[n1, n2], [n3, n4]]
 
     oddsratio, pvalue = stats.fisher_exact(mat, 'greater')
-    return` oddsratio, pvalue
+    return oddsratio, pvalue
 
-
-# In[71]:
-
-
-
-
-
-# In[ ]:
-
-
-scores = []
-mat1 = data.drop("id2peak",axis=1).to_numpy()
-
-interset = set(intersect[:,0])
-
-for i in range (mat1.shape[1]):
-    s1 = set([id2peak[j] for j in np.where(mat1[:, i] > 0.5)[0]])
+def scorefun(input):
     tmp = []
-    for c, s2 in set2_ref:
+    for c, s2 in input[1]:
         s2 = set(s2[:,0])
-        if not (s1 & s2):
+        if not (input[0] & s2):
             tmp.append(0)
         else:
-            tmp.append(get_fisher_exact(s1, s2, interset)[0])
-    scores.append(tmp)
+            tmp.append(get_fisher_exact(input[0], s2, input[2])[0])
+    return tmp
 
+if(multiple):
+    scores = []
+    mat1 = data.drop("id2peak",axis=1).to_numpy()
+    interset = set(intersect[:,0])
+    pool = Pool()
+    for i in range (mat1.shape[1]):
+        s1 = set([id2peak[j] for j in np.where(mat1[:, i] > 0.5)[0]])
+        input = [(s1, set2_ref, interset)]
+        pool.apply_async(scorefun, input, callback=scores.append)
+    pool.close()
+    pool.join()
+else:     
+    scores = []
+    mat1 = data.drop("id2peak",axis=1).to_numpy()
 
-# In[12]:
+    interset = set(intersect[:,0])
 
+    for i in range (mat1.shape[1]):
+        s1 = set([id2peak[j] for j in np.where(mat1[:, i] > 0.5)[0]])
+        tmp = []
+        for c, s2 in set2_ref:
+            s2 = set(s2[:,0])
+            if not (s1 & s2):
+                tmp.append(0)
+            else:
+                tmp.append(get_fisher_exact(s1, s2, interset)[0])
+        scores.append(tmp)
 
 if(verbose): print("Intializing TSNE")
-
-
-# In[13]:
-
-
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
 
 scoresNP = np.array(scores)
 mat = mat1.T / np.sum(mat1.T, axis = 1, keepdims=True)
@@ -197,29 +168,27 @@ if(verbose): clf1 = TSNE(n_components = 2, verbose=1)
 else: clf1 = TSNE(n_components = 2)
 df_tsne = clf1.fit_transform(clf0.fit_transform(mat))
 
-
-# In[14]:
-
-
-fig, axes = plt.subplots(2, 4, figsize = (12, 6))
-
+dist = len(set2_ref)/figure
+if (dist.is_integer()): 
+    dist = int(dist)
+    fig, axes = plt.subplots(dist, figure, figsize = ((figure *4), (dist*3)))
+else:
+    fig, axes = plt.subplots(len(set2_ref), 1, figsize = (3,(len(set2_ref) *4)))
+    figure = 1
+    
 for i, (c, _) in enumerate(set2_ref):
-    ax = axes[i // 4, i % 4]
+    if(figure == 1 or dist == 1): ax = axes[i]
+    else: ax = axes[i // figure, i % figure]
     vmax, vmin = np.percentile(scoresNP[:, i], 99), np.percentile(scoresNP[:, i], 1)
     ax.scatter(df_tsne[:,0], df_tsne[:,1], 
             s = 5, c = scoresNP[:, i], cmap = 'RdBu_r', vmax = vmax, vmin = vmin)
     ax.set_title(c)
     ax.set_xticks([])
     ax.set_yticks([])
-    
-axes[-1, -1].axis('off')
+axes[-1,-1].axis('off')
 
-if(figure): plt.savefig(inP + '.png')
-else: plt.show()
-
-
-# In[ ]:
-
+if(outP): plt.savefig(inP + '.png')
+plt.show()
 
 def output():
     if(verbose): print("Outputing")
@@ -244,17 +213,7 @@ def output():
     IntersectWriter= csv.writer(IntersectFile, delimiter='\t')
     for line in intersect: IntersectWriter.writerow(line)
     sFile.close()
-
-
-# In[ ]:
-
-
 if(verbose): print("Outputting")
-if(output) output()
+if(outP) output()
 
-
-# In[ ]:
-
-
-
-
+sys.exit(0)
